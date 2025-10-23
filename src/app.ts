03f4243ecc5b9877022601cdaf4952222d4d6496
Rafael Rabelo <rafael.rabelo@itarget.com.br>
2025-10-22 23:36:04 -0300

    refactor: update user retrieval endpoint and remove hardcoded user creation

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,7 @@ export const app = fastify()
 
 const prisma = new PrismaClient()
 
-// app.get('/users', async () => {
-//   const users = await prisma.users.findMany()
-//   return users
-// })
-prisma.user
-  .create({
-    data: {
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      password: '123456',
-    },
-  })
-  .then((user) => {
-    console.log(user)
-  })
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
