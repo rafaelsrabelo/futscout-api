877dab2f3ea201a4e219ad2eeaf62ded61164cbc
Rafael Rabelo <rafael.rabelo@itarget.com.br>
2025-10-27 20:13:28 -0300

    fix: improve error handling in register function to throw unexpected errors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import { fastify } from 'fastify'
 import { appRoutes } from './http/routes.js'
+import { ZodError } from 'zod'
 
 export const app = fastify()
 
 app.register(appRoutes, { prefix: '/api' })
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
