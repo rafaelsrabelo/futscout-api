2dac4c943fdc2824e54e06adfd07da2f46a87944
Rafael Rabelo <rafael.rabelo@itarget.com.br>
2025-10-28 12:57:48 -0300

    feat: add email uniqueness validation to register use case
    - Updated the register use case to prevent registration with the same email address.
    - Added a new test case to verify that an EmailAlreadyExistsError is thrown when attempting to register with an existing email.
    - Updated package.json to specify the package manager version.

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register.js'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '../repositories/in-memory/in-merory-users-repository.js'
+import { EmailAlreadyExistsError } from './errors/email-already-exists-error.js'
 
 describe('Register Use Case', () => {
   it('should hash user password upon registration', async () => {
@@ -18,5 +19,28 @@ describe('Register Use Case', () => {
     const isPasswordCorrectlyHashed = await compare('123456', user.password)
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+      role: 'ATHLETE',
+    })
+
+    await expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+        role: 'ATHLETE',
+      }),
+    ).rejects.toBeInstanceOf(EmailAlreadyExistsError)
   })
 })
